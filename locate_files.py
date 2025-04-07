229eb9b9674d6cdb73594f412dfd02496e50427d
Ethan Muldoon <muldoone@oregonstate.edu>
2025-04-07 15:13:56 -0500

    Working on rucio utils

--- locate_files.py
+++ locate_files.py
@@ -29,10 +29,7 @@ def main():
         description='validate metadata consistency and return a list of unique file DIDs')
     parser.add_argument('-q', '--query', help='MetaCat query to find files')
     parser.add_argument('-f', '--filelist', help='a file containing a list of file DIDs')
-    parser.add_argument('-s', '--strict', action='store_true',
-                        help='check additional fields for consistency')
-    parser.add_argument('-a', '--allow', action='store_true',
-                        help='allow missing or duplicate files')
+    parser.add_argument('-c', '--config', help='a configuration file')
     parser.add_argument('-d', '--debug', action='store_true', help='print debug information')
     parser.add_argument('files', nargs=argparse.REMAINDER, help='individual file DIDs')
     args = parser.parse_args()
@@ -40,15 +37,17 @@ def main():
     if args.debug:
         io_utils.set_log_level("DEBUG")
 
+    config = io_utils.read_config(args.config)
+
     flist = io_utils.get_inputs(args.filelist, args.files)
 
     flist = metacat_utils.find_logical_files(
-        query=args.query, filelist=flist, strict=args.strict, allow=args.allow
+        query=args.query, filelist=flist, config=config['validation']
     )
 
     flist = rucio_utils.find_physial_files(flist)
     for file in flist:
-        print (file.paths[0])
+        print (file.paths)
 
 if __name__ == '__main__':
     main()
