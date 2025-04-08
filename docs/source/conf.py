993780f1cea0d710ca85524b1adafaab166ee3dc
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2025-04-07 18:58:56 -0700

    extend path to src

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here.
