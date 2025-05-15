77e8713752f09015b0aee6a840a2d15ea89318db
Ethan Muldoon <muldoone@oregonstate.edu>
2025-05-15 18:38:04 -0500

    Messing with command line entry point

--- src/merge_utils/__main__.py
+++ src/merge_utils/__main__.py
@@ -12,35 +12,40 @@ def main():
 
     parser = argparse.ArgumentParser(
         description='Command line interface for merge_utils')
-    parser.add_argument("function", choices=["list_dids", "list_pfns"], help="Function to execute")
-    parser.add_argument('-c', '--config', help='a configuration file')
-    parser.add_argument('-d', '--debug', action='store_true', help='print debug information')
+    #parser.add_argument("function", choices=["list_dids", "list_pfns"], help="Function to execute")
 
-    args, rest = parser.parse_known_args()
+    subparsers = parser.add_subparsers(metavar='FUNCTION', dest='function', help="function to execute")
+
+    did_parser = subparsers.add_parser('dids', help='list the DIDs of files from MetaCat')
+    did_parser.add_argument('-q', '--query', help='MetaCat query to find files')
+    did_parser.add_argument('-f', '--filelist', help='a file containing a list of file DIDs')
+    did_parser.add_argument('files', nargs=argparse.REMAINDER, help='individual file DIDs')
+
+    pfn_parser = subparsers.add_parser('pfns', help='list the PFNs of files from Rucio')
+    pfn_parser.add_argument('-q', '--query', help='MetaCat query to find files')
+    pfn_parser.add_argument('-f', '--filelist', help='a file containing a list of file DIDs')
+    pfn_parser.add_argument('-a', '--all', action='store_true', help='list replicas from all RSEs')
+    pfn_parser.add_argument('files', nargs=argparse.REMAINDER, help='individual file DIDs')
+
+    parser.add_argument('-c', '--config', help='a configuration file')
+    parser.add_argument('-v', '--verbose', action='count', default=0, help='print more verbose output')
+
+    args = parser.parse_args()
     io_utils.setup_log(args.function)
     config.load(args.config)
-    if args.debug:
+    if args.verbose == 1:
+        io_utils.set_log_level("INFO")
+    elif args.verbose > 1:
         io_utils.set_log_level("DEBUG")
 
-    if args.function == "list_dids":
-        list_dids(rest)
-    elif args.function == "list_pfns":
-        list_pfns(rest)
+    if args.function == "dids":
+        list_dids(args)
+    elif args.function == "pfns":
+        list_pfns(args)
 
 
-def list_dids(arg_list: list = None):
+def list_dids(args):
     """List the DIDs of files from MetaCat"""
-
-    parser = argparse.ArgumentParser(
-        description='validate metadata consistency and return a list of unique file DIDs')
-    parser.add_argument('-q', '--query', help='MetaCat query to find files')
-    parser.add_argument('-f', '--filelist', help='a file containing a list of file DIDs')
-    parser.add_argument('files', nargs=argparse.REMAINDER, help='individual file DIDs')
-
-    if arg_list:
-        args = parser.parse_args(arg_list)
-    else:
-        args = parser.parse_args()
 
     flist = io_utils.get_inputs(args.filelist, args.files)
 
@@ -49,21 +54,8 @@ def list_dids(arg_list: list = None):
     for file in retriever.files:
         print (file.did)
 
-def list_pfns(arg_list: list = None):
+def list_pfns(args):
     """List the PFNs of files from Rucio"""
-
-    parser = argparse.ArgumentParser(
-        description='validate metadata consistency and return a list of unique file DIDs')
-    parser.add_argument('-q', '--query', help='MetaCat query to find files')
-    parser.add_argument('-f', '--filelist', help='a file containing a list of file DIDs')
-    parser.add_argument('-a', '--all', action='store_true', help='list replicas from all RSEs')
-    parser.add_argument('files', nargs=argparse.REMAINDER, help='individual file DIDs')
-
-    if arg_list:
-        args = parser.parse_args(arg_list)
-    else:
-        args = parser.parse_args()
-
     flist = io_utils.get_inputs(args.filelist, args.files)
 
     retriever = rucio_utils.RucioRetriever(
