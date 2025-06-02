2809516db02a0ca9e17224b51ac8473672d18d93
Ethan Muldoon <muldoone@oregonstate.edu>
2025-06-02 17:24:40 -0500

    cleaning up tmp files

--- tmp/20250602T205521/tier0.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# This script will submit the JustIN jobs for tier 0
-justin simple-workflow --monte-carlo 2 --jobscript /nashome/e/emuldoon/merge-utils/src/merge_utils/merge.jobscript --env MERGE_CONFIG="tier0_CERN" --env CONFIG_DIR="/cvmfs/fifeuser2.opensciencegrid.org/sw/dune/6bb16fb86136479f53a36d0cb0295be2c67d0c1b" --env OUT_DIR="/nashome/e/emuldoon/scratch" --site CERN
