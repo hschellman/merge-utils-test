380ba2b976f551a5e00568227da1fad748c84e27
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2025-06-07 11:28:24 -0700

    fixes for docs + beamfiles config

--- docs/makeDiagram.sh
+++ docs/makeDiagram.sh
@@ -5,8 +5,10 @@ echo $PWD
 pydeps metacat_utils.py -T png --include-missing --cluster --max-bacon 2 --rankdir BT
 pydeps io_utils.py -T png --include-missing --cluster --max-bacon 2 --rankdir BT
 pydeps merge_set.py -T png --include-missing --cluster --max-bacon 2 --rankdir BT
+pydeps merge_rse.py -T png --include-missing --cluster --max-bacon 2 --rankdir BT
 pydeps rucio_utils.py -T png --include-missing --cluster --max-bacon 2 --rankdir BT
 pydeps justin_utils.py -T png --include-missing --cluster --max-bacon 2 --rankdir BT
+pydeps merge_utils.py -T png --include-missing --cluster --max-bacon 2 --rankdir BT
 #mv file_utils.png $HERE/docs
 #mv rucio_utils.png $HERE/docs
 chmod +x *.png
